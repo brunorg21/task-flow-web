6ab3ad2b3d58a5e6767eef754ed4aa775f31166e
brunorg21 <rbrunog21@gmail.com>
2024-06-24 23:01:57 -0300

    feat: add details button component

--- src/components/details-button.tsx
+++ src/components/details-button.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Ellipsis, Eye } from "lucide-react";
 import {
   DropdownMenu,
@@ -5,34 +7,43 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { Dialog, DialogTrigger } from "./ui/dialog";
+import { Dialog } from "./ui/dialog";
 
 import { Button } from "./ui/button";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 interface DetailsButtonProps {
   modal: ReactNode;
 }
 
 export function DetailsButton({ modal }: DetailsButtonProps) {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger className="hover:bg-secondary rounded-md p-1">
-        <Ellipsis />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent side="right">
-        <DropdownMenuItem asChild className="flex gap-2 items-center">
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button className="flex items-center gap-2" variant={"ghost"}>
-                <Eye className="h-6 w-6" />
-                Visualizar
-              </Button>
-            </DialogTrigger>
-            {modal}
-          </Dialog>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="hover:bg-secondary rounded-md p-1">
+          <Ellipsis />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent side="right">
+          <DropdownMenuItem
+            asChild
+            className="flex gap-2 items-center justify-center"
+          >
+            <Button
+              onClick={() => setIsModalOpen(true)}
+              className="flex items-center justify-center gap-2 w-full"
+              variant={"ghost"}
+            >
+              <Eye className="h-6 w-6" />
+              Visualizar
+            </Button>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <Dialog open={isModalOpen} onOpenChange={() => setIsModalOpen(false)}>
+        {modal}
+      </Dialog>
+    </>
   );
 }
