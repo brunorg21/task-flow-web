09a2d5164e96d235f70640e0c1bf4c1ea3c4eb51
brunorg21 <rbrunog21@gmail.com>
2024-06-24 23:01:02 -0300

    feat: add sign up form component

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,23 +1,24 @@
+import Link from "next/link";
+import { SignUpForm } from "./sign-up-form";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 
 export default function SignIn() {
   return (
-    <div className="flex items-center justify-center h-full bg-muted">
+    <div className="flex flex-col items-center justify-center h-full bg-muted">
       <div className="flex flex-col w-[400px] p-4 space-y-4">
         <div className="text-center text-2xl font-semibold space-y-2">
           Crie sua conta
           <p className="text-sm font-normal">Acompanhe suas tarefas</p>
         </div>
 
-        <form className="flex flex-col space-y-4" action="">
-          <Input placeholder="Nome" />
-          <Input placeholder="Email" />
-          <Input placeholder="Senha" />
-
-          <Button variant={"default"}>Cadastrar</Button>
-        </form>
+        <SignUpForm />
       </div>
+      <span className="flex items-center gap-2">
+        Não possui conta?
+        <Button>
+          <Link href={"/sign-in"}>Crie agora</Link>
+        </Button>
+      </span>
     </div>
   );
 }
---
+++ src/app/(auth)/sign-up/sign-up-form.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { api } from "@/lib/api";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const signUpFormSchema = z.object({
+  username: z.string(),
+  password: z.string().min(5, {
+    message: "Senha deve conter no minímo 5 caracteres",
+  }),
+  email: z.string().email({ message: "Insira um e-mail válido." }),
+});
+
+export type SignUpFormType = z.infer<typeof signUpFormSchema>;
+
+export function SignUpForm() {
+  const { register, handleSubmit, formState } = useForm<SignUpFormType>({
+    resolver: zodResolver(signUpFormSchema),
+  });
+
+  async function handleRegister({ email, password, username }: SignUpFormType) {
+    await api("/user", {
+      method: "POST",
+
+      body: JSON.stringify({
+        email,
+        password,
+        username,
+      }),
+    });
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit(handleRegister)}
+      className="flex flex-col space-y-4"
+      action=""
+    >
+      <Input {...register("username")} name="name" placeholder="Nome" />
+      {formState.errors.username && (
+        <span className="text-sm text-red-400">
+          {formState.errors.username.message}
+        </span>
+      )}
+      <Input
+        {...register("email")}
+        type="email"
+        name="email"
+        placeholder="Email"
+      />
+      {formState.errors.email && (
+        <span className="text-sm text-red-400">
+          {formState.errors.email.message}
+        </span>
+      )}
+      <Input
+        {...register("password")}
+        type="password"
+        name="password"
+        placeholder="Senha"
+      />
+      {formState.errors.password && (
+        <span className="text-sm text-red-400">
+          {formState.errors.password.message}
+        </span>
+      )}
+
+      <Button variant={"default"}>Cadastrar</Button>
+    </form>
+  );
+}
