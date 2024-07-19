7e7574f9846bc9db046fdb3bf021161c2f8ac9d1
brunorg21 <rbrunog21@gmail.com>
2024-07-19 12:56:06 -0300

    feat: add current user on menu

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,33 +1,33 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
-  DropdownMenuPortal,
   DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useAuth } from "@/providers/auth-provider";
 import { CircleUser, LogOut } from "lucide-react";
 
 export function AccountMenu() {
+  const { user } = useAuth();
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button className="flex gap-2 items-center" variant="outline">
           <CircleUser size={20} />
-          Perfil
+          {user ? user.username : "Perfil"}
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56">
         <DropdownMenuLabel>Minha conta</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem className="flex gap-2">Bruno Rafael</DropdownMenuItem>
+        <DropdownMenuItem className="flex gap-2">
+          {user?.username}
+        </DropdownMenuItem>
         <DropdownMenuItem className="flex gap-2">
           <LogOut size={15} />
           Sair
