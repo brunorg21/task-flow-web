d5dc7b1aedea0d25d47d5669cffa4904231a24ea
brunorg21 <rbrunog21@gmail.com>
2024-07-19 12:55:08 -0300

    refactor: change board-card to receive dynamic data

--- src/components/board-card.tsx
+++ src/components/board-card.tsx
@@ -1,4 +1,4 @@
-import { Star } from "lucide-react";
+import { ListChecks } from "lucide-react";
 import {
   Card,
   CardContent,
@@ -8,22 +8,31 @@ import {
 } from "./ui/card";
 import Link from "next/link";
 
-export function BoardCard() {
+interface BoardCardProps {
+  boardName: string;
+  userBoard?: boolean;
+  boardId: string;
+}
+
+export function BoardCard({
+  boardName,
+  userBoard = false,
+  boardId,
+}: BoardCardProps) {
   return (
-    <Link className="col-span-2" href={"/taskflow/tasks/user"}>
+    <Link
+      className="col-span-2"
+      href={`/taskflow/tasks/${userBoard ? "user" : "org"}/${boardId}`}
+    >
       <Card className="bg-primary-foreground md:hover:scale-105 hover:scale-[102%] duration-150">
         <CardHeader>
-          <CardTitle className="text-lg">Gerencie suas tarefas</CardTitle>
+          <CardTitle className="flex items-center gap-2 text-lg">
+            <ListChecks className="w-6 h-6" /> Gerencie suas tarefas
+          </CardTitle>
         </CardHeader>
-        <CardContent>
-          <div className="flex items-center gap-2">
-            <Star fill="yellow" color="yellow" className="w-5 h-5" />{" "}
-            <span className="text-sm font-bold">Bruno Rafael</span>
-          </div>
-        </CardContent>
-        <CardFooter className="flex justify-end text-sm gap-1">
-          <span className="font-bold text-yellow-200">46</span> tarefas
-          pendentes
+        <CardContent></CardContent>
+        <CardFooter className="flex justify-start text-sm gap-1">
+          <span className="text-lg font-semibold">{boardName}</span>
         </CardFooter>
       </Card>
     </Link>
