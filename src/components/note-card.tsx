3a4551a4277a0c71078596fb7477df54a6ffb506
brunorg21 <rbrunog21@gmail.com>
2024-09-15 22:36:50 -0300

    feat: add fetch annotations on note drawer

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,32 +1,22 @@
-import { Paperclip } from "lucide-react";
-import { FileView } from "./file-view";
+import { format } from "date-fns";
 import { Card, CardContent, CardHeader } from "./ui/card";
-import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Note } from "@/types/note";
 
-export function NoteCard() {
+interface NoteCardProps {
+  annotation: Note;
+}
+
+export function NoteCard({ annotation }: NoteCardProps) {
   return (
     <Card>
-      <CardHeader className="flex flex-row justify-between">
-        Nota 1
-        <Popover>
-          <PopoverTrigger
-            title="Anexos"
-            className="hover:bg-primary-foreground rounded-full p-1"
-          >
-            <Paperclip />
-          </PopoverTrigger>
-          <PopoverContent className="space-y-4">
-            <FileView />
-            <FileView />
-            <FileView />
-          </PopoverContent>
-        </Popover>
+      <CardHeader className="flex flex-row items-center justify-between text-md">
+        {annotation.user.username}
+        <div className="text-xs">
+          {format(annotation.createdAt, "dd/MM/yyyy")}
+        </div>
       </CardHeader>
-      <CardContent className="flex flex-col gap-4">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus harum
-        est soluta amet! Repellat, sequi qui vitae nam sed inventore accusantium
-        deleniti molestias nemo provident aspernatur molestiae beatae ducimus
-        sunt.
+      <CardContent className="flex flex-col gap-4 text-sm">
+        {annotation.description}
       </CardContent>
     </Card>
   );
